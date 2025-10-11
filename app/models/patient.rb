461c769f6a3b8ae78a87aacf8225410df466cddd
Joost Van Averbeke <joost.van.averbeke@gmail.com>
2025-10-11 16:27:34 +0200

    Added Patient model validation + improve patient views

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,9 @@
 class Patient < ApplicationRecord
   belongs_to :municipality
+  validates :first_name, presence: true
+  validates :surname, presence: true
+  validates :birth_date, presence: true
+  validates :gender, presence: true
+
+  enum :gender, { male: 0, female: 1, other: 2 }
 end
