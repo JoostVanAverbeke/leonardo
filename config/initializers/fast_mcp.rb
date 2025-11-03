fc2e3162656e4823ec325af7a9f17c132956d090
Joost Van Averbeke <Joost.VanAverbeke@clinisys.com>
2025-11-03 13:53:37 +0100

    Added RAILS_DEVELOPMENT_HOSTS, allow MCP for other hosts then localhost

--- config/initializers/fast_mcp.rb
+++ config/initializers/fast_mcp.rb
@@ -23,7 +23,7 @@ FastMcp.mount_in_rails(
   sse_route: "sse", # This is the default route for the SSE endpoint
   # Add allowed origins below, it defaults to Rails.application.config.hosts
   # allowed_origins: ['localhost', '127.0.0.1', '[::1]', 'example.com', /.*\.example\.com/],
-  # localhost_only: true, # Set to false to allow connections from other hosts
+  localhost_only: false, # Set to false to allow connections from other hosts
   # whitelist specific ips to if you want to run on localhost and allow connections from other IPs
   # allowed_ips: ['127.0.0.1', '::1'],
   # authenticate: true,       # Uncomment to enable authentication
